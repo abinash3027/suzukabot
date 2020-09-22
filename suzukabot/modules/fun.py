4245ff6f4664ec6f58cee02d9e48dde44d29b30f
Abinash Kushwaha <abinash3027@users.noreply.github.com>
2020-09-22 11:18:44 +0545

    Update fun.py

--- suzukabot/modules/fun.py
+++ suzukabot/modules/fun.py
@@ -3,8 +3,8 @@ import random
 import time
 from typing import List
 
-from telegram import Bot, Update, ParseMode, ChatPermissions
-from telegram.ext import CallbackContext, run_async
+from telegram import Bot, Update, ParseMode
+from telegram.ext import run_async
 
 import suzukabot.modules.fun_strings as fun_strings
 from suzukabot import dispatcher
@@ -12,25 +12,13 @@ from suzukabot.modules.disable import DisableAbleCommandHandler
 from suzukabot.modules.helper_funcs.chat_status import is_user_admin, user_admin
 from suzukabot.modules.helper_funcs.extraction import extract_user
 
-GIF_ID = 'CgACAgQAAx0CSVUvGgAC7KpfWxMrgGyQs-GUUJgt-TSO8cOIDgACaAgAAlZD0VHT3Zynpr5nGxsE'
-
-
 @run_async
-def runs(update: Update, context: CallbackContext):
+def runs(bot: Bot, update: Update):
     update.effective_message.reply_text(random.choice(fun_strings.RUN_STRINGS))
 
 
 @run_async
-def sanitize(update: Update, context: CallbackContext):
-    message = update.effective_message
-    name = message.reply_to_message.from_user.first_name if message.reply_to_message else message.from_user.first_name
-    reply_animation = message.reply_to_message.reply_animation if message.reply_to_message else message.reply_animation
-    reply_animation(GIF_ID, caption=f'*Sanitizes {name}*')
-
-
-@run_async
-def slap(update: Update, context: CallbackContext):
-    bot, args = context.bot, context.args
+def slap(bot: Bot, update: Update, args: List[str]):
     message = update.effective_message
     chat = update.effective_chat
 
@@ -49,11 +37,7 @@ def slap(update: Update, context: CallbackContext):
                     return
 
                 mutetime = int(time.time() + 60)
-                bot.restrict_chat_member(
-                    chat.id,
-                    message.from_user.id,
-                    until_date=mutetime,
-                    permissions=ChatPermissions(can_send_messages=False))
+                bot.restrict_chat_member(chat.id, message.from_user.id, until_date=mutetime, can_send_messages=False)
             reply_text(temp[0])
         else:
             reply_text(temp)
@@ -74,40 +58,37 @@ def slap(update: Update, context: CallbackContext):
     hit = random.choice(fun_strings.HIT)
     throw = random.choice(fun_strings.THROW)
 
-    reply = temp.format(
-        user1=user1, user2=user2, item=item, hits=hit, throws=throw)
+    reply = temp.format(user1=user1, user2=user2, item=item, hits=hit, throws=throw)
 
     reply_text(reply, parse_mode=ParseMode.HTML)
 
 
 @run_async
-def roll(update: Update, context: CallbackContext):
+def roll(bot: Bot, update: Update):
     update.message.reply_text(random.choice(range(1, 7)))
 
 
 @run_async
-def toss(update: Update, context: CallbackContext):
+def toss(bot: Bot, update: Update):
     update.message.reply_text(random.choice(fun_strings.TOSS))
 
 
 @run_async
-def shrug(update: Update, context: CallbackContext):
+def shrug(bot: Bot, update: Update):
     msg = update.effective_message
     reply_text = msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
     reply_text(r"¯\_(ツ)_/¯")
 
 
 @run_async
-def bluetext(update: Update, context: CallbackContext):
+def bluetext(bot: Bot, update: Update):
     msg = update.effective_message
     reply_text = msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
-    reply_text(
-        "/BLUE /TEXT\n/MUST /CLICK\n/I /AM /A /STUPID /ANIMAL /THAT /IS /ATTRACTED /TO /COLORS"
-    )
+    reply_text("/BLUE /TEXT\n/MUST /CLICK\n/I /AM /A /STUPID /ANIMAL /THAT /IS /ATTRACTED /TO /COLORS")
 
 
 @run_async
-def rlg(update: Update, context: CallbackContext):
+def rlg(bot: Bot, update: Update):
     eyes = random.choice(fun_strings.EYES)
     mouth = random.choice(fun_strings.MOUTHS)
     ears = random.choice(fun_strings.EARS)
@@ -120,35 +101,30 @@ def rlg(update: Update, context: CallbackContext):
 
 
 @run_async
-def decide(update: Update, context: CallbackContext):
+def decide(bot: Bot, update: Update):
     reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
     reply_text(random.choice(fun_strings.DECIDE))
 
-
 @run_async
-def table(update: Update, context: CallbackContext):
+def table(bot: Bot, update: Update):
     reply_text = update.effective_message.reply_to_message.reply_text if update.effective_message.reply_to_message else update.effective_message.reply_text
     reply_text(random.choice(fun_strings.TABLE))
 
 
 __help__ = """
- • `/runs`*:* reply a random string from an array of replies
- • `/slap`*:* slap a user, or get slapped if not a reply
- • `/shrug`*:* get shrug XD
- • `/table`*:* get flip/unflip :v
+ • `/runs`*:* reply a random string from an array of replies.
+ • `/slap`*:* slap a user, or get slapped if not a reply.
+ • `/shrug`*:* get shrug XD.
+ • `/table`*:* get flip/unflip :v.
  • `/decide`*:* Randomly answers yes/no/maybe
  • `/toss`*:* Tosses A coin
  • `/bluetext`*:* check urself :V
- • `/roll`*:* Roll a dice
+ • `/roll`*:* Roll a dice.
  • `/rlg`*:* Join ears,nose,mouth and create an emo ;-;
- • `/shout <keyword>`*:* write anything you want to give loud shout
- • `/weebify <text>`*:* returns a weebified text
- • `/sanitize`*:* always use this before /pat or any contact
- """
+"""
 
-SANITIZE_HANDLER = DisableAbleCommandHandler("sanitize", sanitize)
 RUNS_HANDLER = DisableAbleCommandHandler("runs", runs)
-SLAP_HANDLER = DisableAbleCommandHandler("slap", slap)
+SLAP_HANDLER = DisableAbleCommandHandler("slap", slap, pass_args=True)
 ROLL_HANDLER = DisableAbleCommandHandler("roll", roll)
 TOSS_HANDLER = DisableAbleCommandHandler("toss", toss)
 SHRUG_HANDLER = DisableAbleCommandHandler("shrug", shrug)
@@ -157,7 +133,6 @@ RLG_HANDLER = DisableAbleCommandHandler("rlg", rlg)
 DECIDE_HANDLER = DisableAbleCommandHandler("decide", decide)
 TABLE_HANDLER = DisableAbleCommandHandler("table", table)
 
-dispatcher.add_handler(SANITIZE_HANDLER)
 dispatcher.add_handler(RUNS_HANDLER)
 dispatcher.add_handler(SLAP_HANDLER)
 dispatcher.add_handler(ROLL_HANDLER)
@@ -168,12 +143,8 @@ dispatcher.add_handler(RLG_HANDLER)
 dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
 
+
 __mod_name__ = "Fun"
-__command_list__ = [
-    "runs", "slap", "roll", "toss", "shrug", "bluetext", "rlg", "decide",
-    "table", "sanitize"
-]
-__handlers__ = [
-    RUNS_HANDLER, SLAP_HANDLER, ROLL_HANDLER, TOSS_HANDLER,
-    SHRUG_HANDLER, BLUETEXT_HANDLER, RLG_HANDLER, DECIDE_HANDLER, TABLE_HANDLER, SANITIZE_HANDLER
-]
+__command_list__ = ["runs", "slap", "roll", "toss", "shrug", "bluetext", "rlg", "decide", "table"]
+__handlers__ = [RUNS_HANDLER, SLAP_HANDLER, ROLL_HANDLER, TOSS_HANDLER, SHRUG_HANDLER, BLUETEXT_HANDLER, RLG_HANDLER,
+                DECIDE_HANDLER, TABLE_HANDLER]
